02d935c773be86cc3281c2a1c80cd45dd57d9c40
Alexs <alexsdev@outlook.fr>
2025-09-26 11:34:26 +0200

    Mobile

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const cube = document.querySelector('.cube');
 
 let dragging = false;
 let startX, startY;
-let rotationX = 0;
-let rotationY = 0;
+let rotationX = -30;
+let rotationY = 15;
 
 document.addEventListener('mousedown', (e) => {
   dragging = true;
@@ -29,3 +29,39 @@ document.addEventListener('mousemove', (e) => {
   startX = e.clientX;
   startY = e.clientY;
 });
+
+document.addEventListener('touchstart', (e) => {
+  dragging = true;
+  startX = e.clientX;
+  startY = e.clientY;
+});
+
+document.addEventListener('touchend', () => {
+  dragging = false;
+});
+
+document.addEventListener('touchmove', (e) => {
+  if (!dragging) return;
+
+  let endX = e.clientX - startX;
+  let endY = e.clientY - startY;
+
+  rotationY += endX * 0.3;
+  rotationX -= endY * 0.3;
+
+  cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+
+  startX = e.clientX;
+  startY = e.clientY;
+});
+
+function animate() {
+  if (!dragging) {
+    rotationX += 0.2;
+    rotationY += 0.2;
+    cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
